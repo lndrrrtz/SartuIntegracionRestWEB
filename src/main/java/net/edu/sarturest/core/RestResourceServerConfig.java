6517459a47cff97b3702cdea273f242a51a89b9b
lndr <landerurritza446@comunidadunir.net>
2024-07-17 17:03:07 +0200

    Cambio configuración log4j de lugar

--- src/main/java/net/edu/sarturest/core/RestResourceServerConfig.java
+++ src/main/java/net/edu/sarturest/core/RestResourceServerConfig.java
@@ -25,22 +25,39 @@ public class RestResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Value("${oauth2.checktokenuri}")
 	private String checktokenuri;
 	
+	/**
+	 * Sobrescribe el método configure para asignar la audiencia "Sartu" y solo permitir la autorización
+	 * mediante tokens JWT que tienen esa audiencia
+	 * 
+	 * @throws Exception si ocurre un error en la configuración.
+	 */
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
 		resources.resourceId("Sartu");
 	}
 	
+	/**
+	 * Configura la seguridad web, incluyendo el inicio de sesión y el cierre de sesión.
+	 * 
+	 * @param http el objeto {@link HttpSecurity} utilizado para construir la configuración de seguridad.
+	 * @throws Exception si ocurre un error en la configuración de la seguridad web.
+	 */
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http
 			.antMatcher("/**")
 			.authorizeRequests()
-//				.antMatchers("/api/v1/usuarios").permitAll()
 				.antMatchers("/api/v1/usuarios").access("#oauth2.isClient() && #oauth2.hasScope('read') && #oauth2.clientHasRole('CONSULTA')")
-//				.antMatchers("/api/v1/clientes").access("#oauth2.isClient() && #oauth2.hasScope('read') && #oauth2.clientHasRole('CONSULTA')")
+				.antMatchers("/api/v1/clientes").access("#oauth2.isClient() && #oauth2.hasScope('read') && #oauth2.clientHasRole('CONSULTA')")
+				.antMatchers("/api/v1/restricciones").access("#oauth2.isClient() && #oauth2.hasScope('write') && #oauth2.clientHasRole('RESTRICCIONES')")
 				.antMatchers("/**").fullyAuthenticated();
 	}
 	
+	/**
+	 * Define ResourceServerTokenServices que se encarga de verificar la validez del token JWT recibido en la petición
+	 * 
+	 * @return {@link ResourceServerTokenServices} configurado con datos del cliente y el endpoint de verificación de Sartu (SSO)
+	 */
 	@Bean
 	public ResourceServerTokenServices tokenService() {
 		RemoteTokenServices tokenServices = new RemoteTokenServices();
